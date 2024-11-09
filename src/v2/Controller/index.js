f90fc64f730830812b2d2aca151ea4f8ae218bab
debolinahalder1997 <onlinedebolinahalder97@gmail.com>
2024-11-09 10:49:05 +0530

    secret key hide to env file

--- src/v2/Controller/index.js
+++
@@ -1,7 +0,0 @@
-const AuthController = require("./Auth.Controller");
-const DocumnetController = require("./Documnet.Controller");
-
-module.exports = {
-  AuthController,
-  DocumnetController
-};
--- src/v2/Services/Index.Service.js
+++
@@ -1,8 +0,0 @@
-const LoginService = require("./login.Services.js");
-const UserRegService = require("./User.Reg.Services.js");
-const DocuploadService = require("./Docupload.Services.js");
-module.exports = {
-    LoginService,
-    UserRegService,
-    DocuploadService
-}
